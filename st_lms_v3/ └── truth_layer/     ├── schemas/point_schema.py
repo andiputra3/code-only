1870cfbe7afbd4ef48422ef00fd8882a95b95105
andiputra3 <3andiputra3@gmail.com>
2026-07-09 17:00:04 +0700

    Delete st_lms_v3/ └── truth_layer/     ├── schemas directory

--- st_lms_v3/ └── truth_layer/     ├── schemas/point_schema.py
+++
@@ -1,6 +0,0 @@
-class SupertrendPoint:
-    point_id: str       # Contoh: "P2607081920" (Prefix + YYMMDDHHmm)
-    price: float        # Contoh: 61526.8
-    type: str           # Enum: "SUPPORT" | "RESISTANCE"
-    color: str          # Enum: "GREEN" | "RED"
-    time_wib: str       # Contoh: "2026-07-08 19:20:00"
--- st_lms_v3/ └── truth_layer/     ├── schemas/wave_schema.py
+++
@@ -1,5 +0,0 @@
-class SupertrendWave:
-    wave_id: str        # Contoh: "W2607082200"
-    pattern: str        # Enum: "UPTREND_LADDER", "DOWNTREND_LADDER", "SIDEWAY_CHANNEL", "COMPRESSION"
-    members: list       # List of line_id (Anggota Line pembentuk Wave)
-    signature: dict     # Berisi avg_duration, market_mode, color_sequence
